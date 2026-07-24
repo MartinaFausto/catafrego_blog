53505084f98519e33ab5b27a49ce4cdf1f9904e0
martinafausto <martina.fausto@hotmail.it>
2026-07-24 15:52:28 +0200

    focus out menu burger

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,12 +1,10 @@
-import { Component } from '@angular/core';
-import { ToolbarModule } from 'primeng/toolbar';
+import { Component, ElementRef, HostListener, inject } from '@angular/core';import { ToolbarModule } from 'primeng/toolbar';
 import { ButtonModule } from 'primeng/button';
 import { LanguageSelector } from '../language-selector/language-selector';
 import { TagModule } from 'primeng/tag';
 import { RouterLink } from '@angular/router';
 import { TranslatePipe } from '@ngx-translate/core';
 import { ROUTES } from '../../app.routes';
-import { HostListener, inject } from '@angular/core';
 import { NgClass } from '@angular/common';
 
 interface HeaderLink {
@@ -22,12 +20,21 @@ interface HeaderLink {
 })
 export class Header {
 
+    private elementRef = inject(ElementRef);
+
     scrolled = true;
     menuOpen = false;
 
     @HostListener('window:scroll')
     onScroll() {
       //this.scrolled = window.scrollY > 50;
+    }
+
+    @HostListener('document:click', ['$event'])
+    clickOutside(event: MouseEvent) {
+      if (!this.elementRef.nativeElement.contains(event.target)) {
+          this.menuOpen = false;
+      }
     }
 
     links: HeaderLink[] = [
