1d94cd8b1b934bf8e144f6291836c2fa0922f29f
martinafausto <martina.fausto@hotmail.it>
2026-08-05 14:54:07 +0200

    fix

--- src/app/components/timeline/timeline.ts
+++ src/app/components/timeline/timeline.ts
@@ -5,11 +5,11 @@ import { ButtonModule } from 'primeng/button';
 import { CheckCircle } from '@primeicons/angular/check-circle';
 import { MapMarker } from '@primeicons/angular/map-marker';
 import { TranslatePipe } from '@ngx-translate/core';
-import { ExternalLink } from '@primeicons/angular/external-link';
+import { Link } from '@primeicons/angular/link';
 
 @Component({
   selector: 'app-timeline',
-  imports: [AvatarModule, ExternalLink, TimelineModule, ButtonModule, CheckCircle, MapMarker, TranslatePipe],
+  imports: [AvatarModule, Link, TimelineModule, ButtonModule, CheckCircle, MapMarker, TranslatePipe],
   templateUrl: './timeline.html',
   styleUrl: './timeline.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
