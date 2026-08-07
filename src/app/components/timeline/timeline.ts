e538eedc91ba5cdfc6c5192d23396d9095559662
martinafausto <martina.fausto@hotmail.it>
2026-08-07 12:10:34 +0200

    colori header

--- src/app/components/timeline/timeline.ts
+++ src/app/components/timeline/timeline.ts
@@ -3,13 +3,12 @@ import { AvatarModule } from 'primeng/avatar';
 import { TimelineModule } from 'primeng/timeline';
 import { ButtonModule } from 'primeng/button';
 import { CheckCircle } from '@primeicons/angular/check-circle';
-import { MapMarker } from '@primeicons/angular/map-marker';
 import { TranslatePipe } from '@ngx-translate/core';
 import { Link } from '@primeicons/angular/link';
 
 @Component({
   selector: 'app-timeline',
-  imports: [AvatarModule, Link, TimelineModule, ButtonModule, CheckCircle, MapMarker, TranslatePipe],
+  imports: [AvatarModule, Link, TimelineModule, ButtonModule, CheckCircle, TranslatePipe],
   templateUrl: './timeline.html',
   styleUrl: './timeline.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
